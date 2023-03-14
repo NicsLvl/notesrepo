15fb5fc7b5e21942d7f36ed8195bcde0bc31937a
NicsLvl <94282946+NicsLvl@users.noreply.github.com>
2023-03-14 09:10:50 +0800

    Updated for 13 Mar 2023

---
+++ my_functions.py
@@ -0,0 +1,11 @@
+import urllib
+from urllib.parse import urlparse
+import httplib2 as http
+import json
+
+import pandas as pd
+
+class MyFunctions:
+    
+        def __init__(self):
+            pass
